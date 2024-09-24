ed248d8609b9148f0f6a66f04733b6d2a9d6bace
Hugo Bohácsek <xbohach00@stud.fit.vutbr.cz>
2024-09-24 21:09:57 +0200

    update s_var handlers

--- main.js
+++ main.js
@@ -74,6 +74,16 @@ io.on('connection', (socket) => {
 			socket.emit("error", `Error sending command (${error.code})`);
 		});
 	});
+
+	socket.on("s_var_push", (data) => {
+		// bez bodky
+		let addresses = []
+		Object.keys(data).forEach(script_path => {
+			addresses.push(`${hangair_ip}:8080/${script_path.split(".")[0]}`)
+		})
+		console.log(addresses)
+		sendRequests(addresses, data, socket);
+	});
 });
 
 // Start the server
@@ -83,59 +93,111 @@ server.listen(port, () => {
 });
 
 async function fetchDroneVideo() {
-    try {
-        const response = await axios({
-            method: 'get',
-            url: `${hangair_ip}/drone_video`,
-            responseType: 'stream'
-        });
+	try {
+		const response = await axios({
+			method: 'get',
+			url: `${hangair_ip}/drone_video`,
+			responseType: 'stream'
+		});
 
-        response.data.on('data', (chunk) => {
-            io.emit('drone_video', chunk.toString('base64'));
-        });
+		response.data.on('data', (chunk) => {
+			io.emit('drone_video', chunk.toString('base64'));
+		});
 
-        response.data.on('end', () => {
-            console.log('Stream ended');
-        });
-    } catch (error) {
-        //console.error('Error fetching video stream:', error, "Waiting for 10 seconds before retrying...");
+		response.data.on('end', () => {
+			console.log('Stream ended');
+		});
+	} catch (error) {
+		//console.error('Error fetching video stream:', error, "Waiting for 10 seconds before retrying...");
 		setTimeout(fetchDroneVideo, 10000);
-    }
+	}
 }
 fetchDroneVideo();
 
 async function fetchHangairVideos() {
-    try {
-        const video1 = await axios({
-            method: 'get',
-            url: `${hangair_ip}/hangair_video_1`,
-            responseType: 'stream'
-        });
+	try {
+		const video1 = await axios({
+			method: 'get',
+			url: `${hangair_ip}/hangair_video_1`,
+			responseType: 'stream'
+		});
 
-        video1.data.on('data', (chunk) => {
-            io.emit('drone_video', chunk.toString('base64'));
-        });
+		video1.data.on('data', (chunk) => {
+			io.emit('drone_video', chunk.toString('base64'));
+		});
 
-        video1.data.on('end', () => {
-            console.log('Stream ended');
-        });
+		video1.data.on('end', () => {
+			console.log('Stream ended');
+		});
 
 		const video2 = await axios({
-            method: 'get',
-            url: `${hangair_ip}/hangair_video_2`,
-            responseType: 'stream'
-        });
+			method: 'get',
+			url: `${hangair_ip}/hangair_video_2`,
+			responseType: 'stream'
+		});
 
-        video2.data.on('data', (chunk) => {
-            io.emit('drone_video', chunk.toString('base64'));
-        });
+		video2.data.on('data', (chunk) => {
+			io.emit('drone_video', chunk.toString('base64'));
+		});
 
-        video2.data.on('end', () => {
-            console.log('Stream ended');
-        });
-    } catch (error) {
-        //console.error('Error fetching video stream:', error, "Waiting for 10 seconds before retrying...");
+		video2.data.on('end', () => {
+			console.log('Stream ended');
+		});
+	} catch (error) {
+		//console.error('Error fetching video stream:', error, "Waiting for 10 seconds before retrying...");
 		setTimeout(fetchHangairVideos, 10000);
-    }
+	}
 }
 fetchHangairVideos();
+
+function applyUpdates(dataStructure, updates) {
+	for (const shortFilename in updates) {
+		const changes = updates[shortFilename];
+		for (const fullPath in dataStructure) {
+			const filename = path.basename(fullPath);
+			if (filename === shortFilename) {
+				for (const key in changes) {
+					if (dataStructure[fullPath][key]) {
+						console.log(`Updating ${key} in ${fullPath} from ${dataStructure[fullPath][key].value} to ${changes[key]}`);
+						dataStructure[fullPath][key].value = changes[key];
+					} else {
+						console.warn(`Warning: ${key} not found in ${fullPath}`);
+					}
+				}
+			}
+		}
+	}
+	return dataStructure;
+}
+
+async function sendRequests(addresses, data, socket) {
+	var cnt = 0;
+	try {
+		const requests = addresses.map(address => {
+			return axios.post(address, Object.values(data)[cnt++])
+				.then(response => {
+					if (response.status === 200){
+						return true;
+					}
+					return false;
+				})
+				.catch(error => {
+					return false;
+				})
+		})
+
+		const results = await Promise.all(requests);
+		const allTrue = results.every(success => success === true);
+
+		if (allTrue){
+			s_vars = applyUpdates(s_vars, data);
+			socket.emit("error", "Applied changes!");
+			io.emit("Warning! s_vars have changed!");
+			io.emit("s_vars", s_vars);
+		} else {
+			socket.emit("error", "Error pushing s_vars!");
+		}
+	} catch (err) {
+		io.emit("error", err.toString());
+	}
+}
